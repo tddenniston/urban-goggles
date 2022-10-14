9257620da2ec766647b6dd5d1bad8d5e001ea571
tddenniston <42841510+tddenniston@users.noreply.github.com>
2022-10-14 13:58:18 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch')
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', async (req, res) => {
+  const response = await fetch('https://ipinfo.io/json');
+  const info = await response.text();
+  const locationInfo = JSON.parse(info);
+
+  res.send(
+    `<head><style>body { font-family: "Segoe UI"; }</style></head>
+    <h1>Hello World from Node Express on Section!!!</h1><p>This app is being served from 4 locations around the world for <b>low latency</b> and <b>high availability</b>: New York, SFO, Amsterdam, Sydney.</p>
+    <p>You are hitting the example application running in ${locationInfo.city}, ${locationInfo.region} - ${locationInfo.country}</p>
+    <p>Distribute your own Node Express app by visiting <a href=\"http://section.io/docs/tutorials/frameworks/nodeexpress/\">http://section.io/docs/tutorials/frameworks/nodeexpress/</a></p>`
+    );
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
